05aff236b7144f1d112b9b96318cba30966547f8
Jeonghun Lee <yi.jeonghun@gmail.com>
2021-04-22 19:25:22 +0900

    menu button, menu div added

--- public/js/control.js
+++ public/js/control.js
@@ -10,6 +10,21 @@ function Control(){
 	var self = this;
 
 	this.Init = function(){
+		self.InitComponentHandle();
 		return self;
+	};
+
+	this.InitComponentHandle = function(){
+		console.log('InitComponentHandle ' );
+		$('#id_btn_menu_open').on('click', self.OpenMenu);
+		$('#id_btn_close_menu').on('click', self.CloseMenu);
+	};
+
+	this.OpenMenu = function(){
+		$('#id_menu_div').show();
+	};
+
+	this.CloseMenu = function(){
+		$('#id_menu_div').hide();
 	};
 }
