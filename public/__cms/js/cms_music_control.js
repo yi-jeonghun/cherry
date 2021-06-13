0f23ebd9d9e421efb85169110ebeba65240cb860
Jeonghun Lee <yi.jeonghun@gmail.com>
2021-06-13 13:26:56 +0900

    cms music - & -> space

--- public/__cms/js/cms_music_control.js
+++ public/__cms/js/cms_music_control.js
@@ -175,6 +175,9 @@ function MusicControl(){
 	this.GetGoogleSearchURL = function(idx){
 		var title = self._music_list[idx].title;
 		var artist = self._music_list[idx].artist;
+		title = title.replace('&' , ' ');
+		artist = artist.replace('&' , ' ');
+		var query = `search?q=lyrics+${encodeURI(title)}+${encodeURI(artist)}`;
 		var query = `search?q=lyrics+${encodeURI(title)}+${encodeURI(artist)}`;
 		query += '&igu=1';
 		var url = 'https://www.google.com/' + query;
